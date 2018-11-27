5c1370fa85237e415d41c3adead40f6c691b0e32
Oliver Tacke <o.tacke@posteo.de>
2018-11-27 14:55:40 +0100

    Update node dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ const config = {
         test: /\.js$/,
         loader: 'babel-loader',
         query: {
-          presets: ['env']
+          presets: ['@babel/env']
         }
       },
       {
